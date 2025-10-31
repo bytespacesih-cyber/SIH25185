c23f177718386d0ed4d9b0879fe8a592689eaaaf
Sharan-Prashanth <sharanprashanth24sp@gmail.com>
2025-10-31 18:16:51 +0530

    [ADD]: Google Analytics

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -3,6 +3,7 @@ import LoadingScreen from "../components/LoadingScreen";
 import "../styles/globals.css";
 import "../styles/editor.css";
 import { useState, useEffect } from "react";
+import Script from 'next/script';
 
 export default function MyApp({ Component, pageProps }) {
   const [loading, setLoading] = useState(true);
@@ -21,8 +22,21 @@ export default function MyApp({ Component, pageProps }) {
   }
 
   return (
+    <>
+      {/* Google Analytics - Measurement ID: G-KJ636M5C63 */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-KJ636M5C63"
+        strategy="afterInteractive"
+      />
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);} 
+gtag('js', new Date());
+gtag('config', 'G-KJ636M5C63', { page_path: window.location.pathname });`}
+      </Script>
     <AuthProvider>
       <Component {...pageProps} />
     </AuthProvider>
+    </>
   );
 }
